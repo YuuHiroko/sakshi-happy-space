54c550e6826c8f92c1b5354880c2956c8d394f65
jamadarahamad003 <jamadarahamad003@gmail.com>
2025-09-24 09:24:31 +0000

    fix: trigger new deployment by updating import

--- src/components/3d/EnhancedBirthdayScene3D.tsx
+++ src/components/3d/EnhancedBirthdayScene3D.tsx
@@ -6,7 +6,7 @@ import BirthdayCake from './BirthdayCake';
 import FloatingBalloons from './FloatingBalloons';
 import ConfettiExplosion from './ConfettiExplosion';
 import GiftBox from './GiftBox';
-import PhotoCarousel3D from './PhotoCarousel3D';
+import PhotoCarousel3D from './PhotoCarousel3D.tsx';
 import SceneLighting from './SceneLighting';
 import SceneFloor from './SceneFloor';
 import GuidedTour from './GuidedTour';
